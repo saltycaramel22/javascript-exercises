b5c7369bea7c98bbb90188bf49f495207e763c7f
saltycaramel22 <arav00422@gmail.com>
2024-08-21 17:12:03 -0400

    Completed.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let rstring = '';
+    for (let s = str.length - 1; s >= 0; s--) {
+        rstring += str[s];
+    }
+    return rstring
 };
+
+console.log(reverseString("hello"));
 
 // Do not edit below this line
 module.exports = reverseString;
