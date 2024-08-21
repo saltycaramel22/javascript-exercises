d330c14b0325f4d328992cd11f804aa246349d20
saltycaramel22 <arav00422@gmail.com>
2024-08-21 14:34:45 -0400

    Completed tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = "";
+    for (let i = 0; i < num; i++) {
+            result += string;
+    }
+    if (num < 0) {
+        return "ERROR";
+    }
+    else {
+        return result;
+    }
 };
+
+console.log(repeatString("hey", -1));
 
 // Do not edit below this line
 module.exports = repeatString;
